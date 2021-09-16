0c10d0c964bb22a50ab74ff03b88f1bd88f74e5f
mashiro <wkq98112581@gmail.com>
2021-09-16 17:59:18 +0800

    vite vue3 ts init

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">this is home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+$width: 5em;
+.test {
+  font-size: $width;
+}
+</style>
