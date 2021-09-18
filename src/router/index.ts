ad0e72731c563f3a641910d39b1c3ec91a3a9938
mashiro <wkq98112581@gmail.com>
2021-09-18 15:53:22 +0800

    练习setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,8 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/login/index.vue')
   }
 ]
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>123</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'login',
+  setup() {}
+})
+</script>
+
+<style scoped></style>
